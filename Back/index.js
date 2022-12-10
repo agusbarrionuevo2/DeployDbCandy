5a746c6587a7c319bac7e5da036d2573263e0a4a
Agustin Barrionuevo <barrionuevoagustin289@gmail.com>
2022-12-10 16:41:11 -0300

    deply

--- Back/index.js
+++ Back/index.js
@@ -1,16 +1,19 @@
 const app = require("./src/app.js");
 const { conn } = require("./src/db");
-const { Port } = require("./src/utils/config");
+const port = process.env.PORT || 3001
 
-(async () => {
-  try {
+// (async () => {
+//   try {
     //Sincronizo todos los modelos con la base de datos.
-    await conn.sync({ force: false }); // { force: true } -> Elimina todos los registros. { alter: true } -> Modifica columnas de la tabla realizando cambios en los registros de tal manera que coincidan con el modelo.
-    console.log("All models were synchronized successfully.");
-    app.listen(Port, () => {
-      console.log("Server listening at port", Port);
-    });
-  } catch (error) {
-    console.log("Error:", error);
-  }
-})();
+    // await 
+    // { force: true } -> Elimina todos los registros. { alter: true } -> Modifica columnas de la tabla realizando cambios en los registros de tal manera que coincidan con el modelo.
+    conn.sync({ force: false }).then(()=> {
+      console.log("All models were synchronized successfully.");
+      app.listen(port, () => {
+        console.log("Server listening at port", port);
+      });
+    })
+//   } catch (error) {
+//     console.log("Error:", error);
+//   }
+// })();
